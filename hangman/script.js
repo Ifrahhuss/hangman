e216e1258cfe5da8fec9ac57da962e7dfd95bb94
Ifrah <ifrahh2017@gmail.com>
2026-07-02 07:35:54 +1000

    fixed the getRandomWord function

--- hangman/script.js
+++ hangman/script.js
@@ -3,8 +3,11 @@ const keyboardDiv = document.querySelector(".keyboard");
 
 const getRandomWord = () => {
     const {word, hint} = wordlist[Math.floor(Math.random()*wordlist.length)];
-    document.querySelector(".hint").innerText = hint;
-    wordDisplay.innerHTML = word.split("").map(() => ``.join(""));
+    document.querySelector(".hint-text").innerText = hint;
+    wordDisplay.innerHTML = word
+    .split("")
+    .map(() => `<li class="letter"></li>`)
+    .join("");
 }
 
 for (let i = 97; i<=122; i++){
