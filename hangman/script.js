4904232f430c6ddbce2910b900e94ee2cabfcd79
Ifrah <ifrahh2017@gmail.com>
2026-07-02 08:55:00 +1000

    if maxWrongGuess reached - gameover

--- hangman/script.js
+++ hangman/script.js
@@ -1,7 +1,11 @@
+const hangManImage = document.querySelector(".hangman-box img");
 const wordDisplay = document.querySelector(".word-display");
+const GuessesText = document.querySelector(".guesses-text");
 const keyboardDiv = document.querySelector(".keyboard");
 
 let currentWord;
+let wrongGuessCount = 0;
+const maxGuess = 10;
 const getRandomWord = () => {
     const {word, hint} = wordlist[Math.floor(Math.random()*wordlist.length)];
     currentWord = word;
@@ -21,6 +25,18 @@ const initGame = (button, clickedletter) => {
         }
     });
     // const letterElements = document.querySelectorAll(".letter");
+} else {
+    wrongGuessCount++;
+    hangManImage.src = `assets/h-${wrongGuessCount}.jpg`;
+    // document.querySelector(".hangman-box img").src = `assets/h-${wrongGuessCount}.jpg`;
+    // document.querySelector(".guesses-text").innerText += clickedletter + " ";
+}
+button.disabled = true;//disables the button so after you click letter, you cant click again
+GuessesText.innerText = `Incorrect Guesses:${wrongGuessCount} / ${maxGuess}`;
+
+if (wrongGuessCount === maxGuess){
+    alert("Game Over! You have reached the maximum number of incorrect guesses.");
+    resetGame();
 }
 }
 
