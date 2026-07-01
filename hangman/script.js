96fd6cb3c425941fddee4d8533f57e7d86046f7f
Ifrah <ifrahh2017@gmail.com>
2026-07-02 08:35:31 +1000

    added function that checks if the correct letter is clicked and makes it appears on screen

--- hangman/script.js
+++ hangman/script.js
@@ -1,8 +1,10 @@
 const wordDisplay = document.querySelector(".word-display");
 const keyboardDiv = document.querySelector(".keyboard");
 
+let currentWord;
 const getRandomWord = () => {
     const {word, hint} = wordlist[Math.floor(Math.random()*wordlist.length)];
+    currentWord = word;
     document.querySelector(".hint-text").innerText = hint;
     wordDisplay.innerHTML = word
     .split("")
@@ -10,9 +12,18 @@ const getRandomWord = () => {
     .join("");
 }
 
-const initGame = (button, letter) => {
-    console.log(button, letter);
+const initGame = (button, clickedletter) => {
+    if (currentWord.includes(clickedletter)){
+        [...currentWord].forEach((l, i) => {
+            if (l === clickedletter){
+                wordDisplay.querySelectorAll("li")[i].innerText = l;
+                wordDisplay.querySelectorAll("li")[i].classList.add("guessed");
+        }
+    });
+    // const letterElements = document.querySelectorAll(".letter");
 }
+}
+
 for (let i = 97; i<=122; i++){
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
